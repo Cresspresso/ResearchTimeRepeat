15d6572a71cb6d28ce4e88349b6ab5679bdb4922
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2020-02-12 14:26:33 +1300

    Inventory Scripts

---
+++ Assets/Scripts/PlayerInventoryController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventoryController : MonoBehaviour
+{
+	public LayerMask worldItemHitMask = ~0;
+	public float maxGrabDistance = 3.0f;
+
+	private Inventory inventory;
+
+	void Awake()
+	{
+		inventory = GetComponent<Inventory>();
+	}
+
+	void Update()
+	{
+		// if player wants to interact with something
+		if (Input.GetButtonDown("Fire1"))
+		{
+			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			// check if they are interacting with a WorldItem
+			if (Physics.Raycast(ray, out var hit, maxGrabDistance, worldItemHitMask))
+			{
+				var worldItem = hit.collider.GetComponentInParent<WorldItem>();
+				if (worldItem)
+				{
+					// if they are interacting with a WorldItem:
+
+					if (worldItem.itemSlot.isEmpty)
+					{
+						throw new System.NullReferenceException("WorldItem's ItemSlot is empty");
+					}
+
+					bool found = false;
+					// Find a hotbar slot that has the same type as the WorldItem
+					for (int i = 0; i < inventory.hotbarSlots.Length; i++)
+					{
+						if (inventory.hotbarSlots[i].type == worldItem.itemSlot.type)
+						{
+							found = true;
+							inventory.hotbarSlots[i] = new ItemSlot(
+								inventory.hotbarSlots[i].type,
+								inventory.hotbarSlots[i].amount + worldItem.itemSlot.amount
+								);
+							worldItem.itemSlot.Clear();
+							Destroy(worldItem.gameObject);
+						}
+					}
+					if (!found)
+					{
+						// Find a hotbar slot that is empty
+						for (int i = 0; i < inventory.hotbarSlots.Length; i++)
+						{
+							if (inventory.hotbarSlots[i].isEmpty)
+							{
+								found = true;
+								// transfer from world item to inventory
+								inventory.hotbarSlots[i] = worldItem.itemSlot.TakeOwnership();
+
+								Destroy(worldItem.gameObject);
+							}
+						}
+						if (!found)
+						{
+							Debug.Log("no empty slots");
+						}
+					}
+				}
+			}
+		}
+	}
+}
