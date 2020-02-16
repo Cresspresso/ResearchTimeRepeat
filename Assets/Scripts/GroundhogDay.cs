aabaa282d62c76764a890f4d849eeae16b869fbd
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2020-02-17 12:16:03 +1300

    Groundhog Day and Timer
    in "elijah" scene

---
+++ Assets/Scripts/GroundhogDay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GroundhogDay : MonoBehaviour
+{
+	public float duration = 20.0f;
+	public float startDelay = 1.0f;
+	public float timeLoopRestartDelay = 1.0f;
+
+	private float m_elapsedTime;
+
+	public float elapsedTime => Mathf.Clamp(m_elapsedTime, 0.0f, duration);
+	public float remainingTime => Mathf.Clamp(duration - m_elapsedTime, 0.0f, duration);
+	public bool hasEnded => m_elapsedTime >= duration;
+
+	private void Start()
+	{
+		m_elapsedTime = -startDelay;
+	}
+
+	private void LateUpdate()
+	{
+		m_elapsedTime += Time.deltaTime;
+		if (m_elapsedTime >= duration + timeLoopRestartDelay)
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+	}
+}
