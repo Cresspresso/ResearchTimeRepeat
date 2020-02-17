7f6f04984e09fa6bc005434ebe0adab5c9b3462a
Lorenzo <lorenzo.zem8065@mediadesign.school.nz>
2020-02-17 16:02:13 +1300

    temp sliding door prefab

---
+++ Assets/slidingdoor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slidingdoor : MonoBehaviour
+{
+    public GameObject door1;
+    public GameObject door2;
+
+    public float waitTime = 2.0f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                door1.SetActive(false);
+                door2.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //if (other.tag == "Player")
+        //{
+        //    door1.SetActive(true);
+        //    door2.SetActive(true);
+        //}
+        StartCoroutine(countdown());
+    }
+
+    IEnumerator countdown()
+    { 
+        yield return new WaitForSeconds(waitTime);
+        door1.SetActive(true);
+        door2.SetActive(true);
+    }
+}
