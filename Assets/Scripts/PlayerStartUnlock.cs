8d04c433428a8a459f3fa44ea43efbfa1ee3ae7f
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2020-02-19 16:39:51 +1300

    prefab clock

--- Assets/Scripts/PlayerStartUnlock.cs
+++ Assets/Scripts/PlayerStartUnlock.cs
@@ -7,6 +7,21 @@ public class PlayerStartUnlock : MonoBehaviour
 	public PlayerController player;
 	public GroundhogDay groundhogDay;
 
+	private void Awake()
+	{
+		if (!groundhogDay)
+		{
+			groundhogDay = FindObjectOfType<GroundhogDay>();
+		}
+		Debug.Assert(groundhogDay, "groundhogDay is null", this);
+
+		if (!player)
+		{
+			player = FindObjectOfType<PlayerController>();
+		}
+		Debug.Assert(player, "player is null", this);
+	}
+
 	private void Start()
 	{
 		player.isGameControlEnabled = false;
