be66d8ffd820c0200b2e0a329f3378ffb3892b98
Lorenzo <lorenzo.zem8065@mediadesign.school.nz>
2020-02-19 14:59:23 +1300

    created simple yRotationScript for emergency lights and ceiling fans

---
+++ Assets/Scripts/PlayerHand/KeyCardReader/DoorOpenerKcr.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(KeyCardReader))]
+public class DoorOpenerKcr : MonoBehaviour
+{
+	public KeyCardReader interactable { get; private set; }
+	public Animator anim;
+
+	private void Awake()
+	{
+		interactable = GetComponent<KeyCardReader>();
+
+		interactable.interactEventComponent.onInteract.AddListener(OnInteract);
+	}
+
+	private void OnDestroy()
+	{
+		interactable.interactEventComponent.onInteract.RemoveListener(OnInteract);
+	}
+
+	private void OnInteract(InteractEventArgs eventArgs)
+	{
+		anim.SetBool("doorOpen", true);
+	}
+}
--- Assets/Scripts/yRotationScript.cs
+++ Assets/Scripts/yRotationScript.cs
@@ -2,14 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class spotlightScript : MonoBehaviour
+public class yRotationScript : MonoBehaviour
 {
     public float rotationSpeed = 100.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
