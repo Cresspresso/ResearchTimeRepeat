15b29664ea76bce273779cbf66dd77a34b2787b2
Lorenzo <lorenzo.zem8065@mediadesign.school.nz>
2020-02-24 09:28:47 +1300

    made respawn trigger prefab

---
+++ Assets/respawnScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawnScript : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 playerStartPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerStartPos = player.transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<CharacterController>().enabled = false;
+            other.transform.position = playerStartPos;
+            other.GetComponent<CharacterController>().enabled = true;
+        }
+    }
+
+}
