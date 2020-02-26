8f108e4dbfa774b696b463c306c3a83b200d6606
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2020-02-26 16:09:49 +1300

    control panel button

---
+++ Assets/Scripts/ControlPanelButton/ControlPanelButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPanelButton : Interactable
+{
+	protected override void OnInteract(InteractEventArgs eventArgs)
+	{
+		Debug.Log("Interacted!", this);
+		base.OnInteract(eventArgs);
+	}
+}
