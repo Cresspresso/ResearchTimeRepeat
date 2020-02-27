3cec567db620405525860fca5a0b3f6bf3d55b3f
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2020-02-27 18:08:57 +1300

    abort button

--- Assets/Scripts/ControlPanelButton/ControlPanelButton.cs
+++ Assets/Scripts/ControlPanelButton/ControlPanelButton.cs
@@ -1,12 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ControlPanelButton : Interactable
 {
+	private bool m_isInteractable = false;
+	public bool isInteractable {
+		get => m_isInteractable;
+		set { m_isInteractable = value; }
+	}
+
+	private void Awake()
+	{
+		isInteractable = isInteractable;
+	}
+
+	public override NotInteractableReason GetNotInteractableReason(InteractEventArgs eventArgs)
+	{
+		if (!m_isInteractable) { return new NotInteractableReason("button disabled"); }
+		return base.GetNotInteractableReason(eventArgs);
+	}
+
 	protected override void OnInteract(InteractEventArgs eventArgs)
 	{
-		Debug.Log("Interacted!", this);
-		base.OnInteract(eventArgs);
+		visuals.SetActive(true);
+		if (anim) { anim.enabled = true; }
+		FindObjectOfType<PlayerController>().isGameControlEnabled = false;
+		//base.OnInteract(eventArgs);
+	}
+
+
+
+
+
+	private Animator anim;
+	public GameObject visuals;
+
+	private void Start()
+	{
+		anim = GetComponent<Animator>();
+		visuals.SetActive(false);
+	}
+
+	private void Update()
+	{
+		int sceneIndex = 2;// "EscapeVideoScene"
+		if (anim)
+		{
+			if (anim.enabled && anim.IsInTransition(0))
+			{
+				var info = anim.GetAnimatorTransitionInfo(0);
+				if (info.IsUserName("CustomTransition"))
+				{
+					SceneManager.LoadScene(sceneIndex);
+				}
+			}
+		}
+		else if (visuals.activeSelf)
+		{
+			SceneManager.LoadScene(sceneIndex);
+		}
 	}
 }
